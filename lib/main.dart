ff2c97d8a38e4cc6464c3e4c5efa734d8e817b62
Samuel Matias <hi@samuelematias.com>
2021-06-12 17:15:56 -0300

    feat(env): add env config

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 import 'app/app.dart';
 
-void main() {
+Future<void> main() async {
+  await DotEnv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const App());
 }
